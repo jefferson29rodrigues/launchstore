205df438fd0208cc9d823d65c6c122cfc201ad75
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-05-04 11:43:56 -0300

    botao remove

--- public/scripts.js
+++ public/scripts.js
@@ -22,17 +22,12 @@ const Mask = {
 
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event) {
         const { files: fileList } = event.target
-        const { uploadLimit } = PhotosUpload
-
-        if(fileList.length > uploadLimit) {
-           alert(`Envie no máximo ${uploadLimit} fotos`)
-
-           event.preventDefault()
-           return
-        }
+        
+        if (PhotosUpload.hasLimit(event)) return
 
         Array.from(fileList).forEach(file => {
             const reader = new FileReader()
@@ -41,17 +36,43 @@ const PhotosUpload = {
                 const image = new Image()
                 image.src = String(reader.result)
 
-                const div = document.createElement('div')
-                div.classList.add('photo')
+                const div = PhotosUpload.getContainer(image)
 
-                div.onclick = () => alert('remover foto')
-
-                div.appendChild(image)
-
-                document.querySelector('#photos-preview').appendChild(div)
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)
         })
+    },
+    hasLimit(event) {
+        const { uploadLimit } = PhotosUpload
+        const { files: fileList } = event.target
+
+        if(fileList.length > uploadLimit) {
+           alert(`Envie no máximo ${uploadLimit} fotos`)
+
+           event.preventDefault()
+           return true
+        }
+
+        return false
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = () => alert('remover foto')
+
+        div.appendChild(image)
+
+        div.appendChild(PhotosUpload.getRemoveButton())
+
+        return div
+    },
+    getRemoveButton() {
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+        return button
     }
 }
